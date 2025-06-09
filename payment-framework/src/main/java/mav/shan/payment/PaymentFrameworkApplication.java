e5c4b090a855ee672b001ff5d9ac95763fbdd5ac
yangyi <1965997927@qq.com>
2025-06-09 15:54:56 +0800

    feat(payment): 添加支付框架应用程序- 新增 PaymentFrameworkApplication 类作为支付框架的入口 - 使用 Spring Boot 构建支付框架的基础

---
+++ payment-framework/src/main/java/mav/shan/payment/PaymentFrameworkApplication.java
@@ -0,0 +1,12 @@
+package mav.shan.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentFrameworkApplication.class, args);
+    }
+}
