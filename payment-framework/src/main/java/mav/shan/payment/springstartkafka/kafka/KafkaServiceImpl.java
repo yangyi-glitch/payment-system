1a2593224fb3f3dd15b9c6a1801a8553b9e12ddf
yangyi <1965997927@qq.com>
2025-06-09 17:46:54 +0800

    feat(payment): 添加 Kafka 配置及主题创建功能
    - 在 application.yml 中添加 Kafka 相关配置
    - 新增 KafkaConfig 类用于获取 Kafka 配置信息
    - 接口 KafkaService 定义了创建主题的方法
    - 实现类 KafkaServiceImpl 提供了具体的主题创建逻辑
    - 在 pom.xml 中添加了 spring-cloud-starter-stream-kafka 依赖

---
+++ payment-framework/src/main/java/mav/shan/payment/springstartkafka/kafka/KafkaServiceImpl.java
@@ -0,0 +1,42 @@
+package mav.shan.payment.springstartkafka.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import mav.shan.payment.springstartkafka.config.KafkaConfig;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Service
+public class KafkaServiceImpl implements KafkaService {
+
+    @Resource
+    private KafkaConfig kafkaConfig;
+
+    @Override
+    public void createTopic(String topicName, int numPartitions, short replicationFactor) {
+        // 创建 AdminClient 的配置
+        Properties props = new Properties();
+        props.put("bootstrap.servers", kafkaConfig.getBootstrapServers());
+
+        // 创建 AdminClient 实例
+        try (AdminClient adminClient = AdminClient.create(props)) {
+            NewTopic newTopic = new NewTopic(topicName, numPartitions, replicationFactor)
+                    .configs(Collections.singletonMap(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_DELETE)); // 设置清理策略
+            // 创建主题
+            adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
+            log.info("主题[{topicName}]已成功创建！");
+        } catch (InterruptedException | ExecutionException e) {
+            log.info("创建主题失败");
+            e.getMessage();
+            log.info("===============================");
+            e.printStackTrace();
+        }
+    }
+}
