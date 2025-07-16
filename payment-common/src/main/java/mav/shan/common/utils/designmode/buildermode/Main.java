42f84dfab5e8c014b0ff7823c2b221efcf215314
yangyi <1965997927@qq.com>
2025-07-16 11:52:38 +0800

    6666

--- payment-common/src/main/java/mav/shan/common/utils/designmode/buildermode/Main.java
+++ payment-common/src/main/java/mav/shan/common/utils/designmode/buildermode/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Computer intel = ComputerBuilder.builder().buildCpu("Intel").buildRam("16G").buildStorage("1T").build();
         Computer amd = ComputerBuilder.builder().buildCpu("AMD").buildRam("16G").buildStorage("1T").build();
-        Computer amd1 = ComputerBuilder.builder().buildCpu("AMD").buildRam("16G").buildStorage("1T").build();
         System.out.println(intel);
         System.out.println(amd);
     }
